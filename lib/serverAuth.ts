2b904d2c64f756b581adf61cb04d8bc2e0e79888
daniel.calderon <daniel.calderon@mxmart.mx>
2023-06-25 21:56:34 -0700

    add/remove favorites

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,13 +1,14 @@
-import { NextApiRequest } from "next";
-import { getSession } from "next-auth/react";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
 
 import prismadb from '@/lib/prismadb';
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
-const serverAuth = async (req: NextApiRequest) => {
-  const session = await getSession({req});
-
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerSession(req, res, authOptions);
+  
   if (!session?.user?.email){
-    throw new Error('Not signed in');
+    throw new Error('Not signed in, email is required');
   }
 
   const currentUser = await prismadb.user.findUnique({
@@ -17,7 +18,7 @@ const serverAuth = async (req: NextApiRequest) => {
   });
 
   if (!currentUser){
-    throw new Error('Not signed in');
+    throw new Error('Not signed in, user does not exist');
   }
 
   return {currentUser};
