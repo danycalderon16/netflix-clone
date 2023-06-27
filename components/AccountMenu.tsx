439abb2823276c3bf09fb963f968fa929cd925ee
daniel.calderon <daniel.calderon@mxmart.mx>
2023-06-26 22:42:18 -0700

    linter

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -17,7 +17,7 @@ const AccountMenu: React.FC<Props> = ({ visible }) => {
         <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
           <img className='w-8 rounded-md' src="/images/default-blue.png" alt="Profile" />
           <p className='text-white text-sm group-hover/item:underline'>
-            {data?.name}
+            {data?.name }
           </p>
         </div>
         <hr className='bg-gray-600 border-0 h-px my-4'/>
